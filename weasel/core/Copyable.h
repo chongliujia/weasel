9344d36740ce458cc0dfc9b4c5395eebad62dc80
chongliujia <jcl314159@gmail.com>
2022-11-03 23:59:18 +0800

    create Copyable.h

---
+++ weasel/core/Copyable.h
@@ -0,0 +1,15 @@
+#ifndef WEASEL_CORE_COPYABLE_H
+#define WEASEL_CORE_COPYABLE_H
+
+
+namespace weasel 
+{
+	class copyable 
+	{
+		protected:
+			copyable() = default;
+			~copyable() = default;
+	};
+}
+
+#endif 
