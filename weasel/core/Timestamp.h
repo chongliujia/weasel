d0823890f2b29b96b6b714c21eb86c40479bc793
chongliujia <jcl314159@gmail.com>
2023-03-19 20:05:06 +0800

    add Timestamp

--- weasel/core/Timestamp.h
+++ weasel/core/Timestamp.h
@@ -14,10 +14,77 @@ namespace weasel
                       public boost::less_than_comparable<Timestemp>
     {
         public:
+            static const int kMicroSecondsPerSecond = 1000 * 1000;
 
+            Timestamp()
+                : microSecondsSinceEpoch_(0)
+            {
+            }
 
+            explicit Timestamp(int64_t microSecondsSinceEpochArg)
+                : microSecondsSinceEpoch_(microSecondsSinceEpochArg)
+            {
+            }
+
+            string toString() const;
+            string toFormattedString(bool showMicroseconds = true) const;
+
+            bool valid() const {
+                return microSecondsSinceEpoch_ > 0;
+            }
+
+            int64_t microSecondsSinceEpoch() const {
+                return microSecondsSinceEpoch_;
+            }
+
+            time_t secondsSinceEpoch() const
+            {
+                return static_cast<time_t> (microSecondsSinceEpoch_ / kMicroSecondsPerSecond);
+            }
+
+            static Timestamp now();
+
+            static Timestamp invalid()
+            {
+                return Timestamp();
+            }
+
+            static Timestamp fromUnixTime(time_t t)
+            {
+                return fromUnixTime(t, 0);
+            }
+
+            static Timestamp fromUnixTime(time_t t, int microseconds)
+            {
+                return Timestamp(static_cast<int64_t>(t) * kMicroSecondsPerSecond + microseconds);
+            }
 
         private:
             int64_t microSecondsSinceEpoch_;
     };
+
+
+    inline bool operator < (Timestamp lhs, Timestamp rhs)
+    {
+        return lhs.microSecondsSinceEpoch() < rhs.microSecondsSinceEpoch();
+    }
+
+    inline bool operator == (Timestamp lhs, Timestamp rhs)
+    {
+        return lhs.microSecondsSinceEpoch() == rhs.microSecondsSinceEpoch();
+    }
+
+    inline double timeDifference(Timestamp high, Timestamp low)
+    {
+        int64_t diff = high.microSecondsSinceEpoch() - low.microSecondsSinceEpoch();
+        return static_cast<double>(diff) / Timestamp::kMircoSecondsPerSecond;
+    }
+
+    inline Timestamp addTime(Timestamp timestamp, double seconds)
+    {
+        int64_t delta = static_cast<int64_t>(seconds * Timestamp::kMicroSecondsPerSecond);
+        return Timestamp(timestamp.microSecondsSinceEpoch() + delta);
+    }
 }
+
+#endif
