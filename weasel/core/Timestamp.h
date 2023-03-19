55a9e858c24aabd99565f9acd8b084231af93f0c
chongliujia <jcl314159@gmail.com>
2023-03-19 19:13:00 +0800

    test

--- weasel/core/Timestamp.h
+++ weasel/core/Timestamp.h
@@ -4,8 +4,20 @@
 #include "Copyable.h"
 #include "Types.h"
 
+#include <boost/operators.hpp>
+
+
 namespace weasel
 {
 	class Timestamp : public weasel::copyable,
-					  public std::equailty_comparable<Timestamp>,
+					  public boost::equailty_comparable<Timestamp>,
+                      public boost::less_than_comparable<Timestemp>
+    {
+        public:
+
+
+
+        private:
+            int64_t microSecondsSinceEpoch_;
+    };
 }
