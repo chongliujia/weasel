4d691af2da3d252c99fd03fe922ffef3b8008e20
chongliujia <jcl314159@gmail.com>
2023-04-02 11:02:12 +0800

    add LogStream

--- weasel/core/LogStream.h
+++ weasel/core/LogStream.h
@@ -1,22 +1,25 @@
-#ifndef WEASEL_CORE_LOGSTREAM_H
-#define WEASEL_CORE_LOGSTREAM_H
+#ifndef  WEASEL_CORE_LOGSTREAM_H
+#define  WEASEL_CORE_LOGSTREAM_H
 
-#include "noncopyable.h"
+#include "Noncopyable.h"
 #include "StringPiece.h"
 #include "Types.h"
 
 #include <assert.h>
 #include <string.h>
 
-namespace weasel 
+const int BUFFER_SIZE = 32;
+
+namespace weasel
 {
     namespace detail
     {
         const int kSmallBuffer = 4000;
         const int kLargeBuffer = 4000 * 1000;
 
+
         template <int SIZE>
-        class FixedBuffer : noncopyable
+        class FixedBuffer : noncopyable 
         {
             public:
                 FixedBuffer()
@@ -32,39 +35,46 @@ namespace weasel
 
                 void append(const char* buf, size_t len)
                 {
-                    if (implicit_cast<size_t>(avail()) > len) {
+                    if (implicit_cast<size_t> (avail()) > len) {
                         memcpy(cur_, buf, len);
                         cur_ += len;
                     }
                 }
 
                 const char* data() const { return data_; }
-                const char* debugString();
+
+                int length() const { return static_cast<int>(cur_ - data_); }
 
                 char* current() { return cur_; }
 
-                int length() const { return static_cast<int>(cur_ - data_); }
                 int avail() const { return static_cast<int>(end() - cur_); }
 
                 void add(size_t len) { cur_ += len; }
                 void reset() { cur_ = data_; }
                 void bzero() { memZero(data_, sizeof(data_)); }
+
+                // for used by GDB
+                char char* debugString();
                 void setCookie(void (*cookie)()) { cookie_ = cookie; }
 
+                // for used by unit test
                 string toString() const { return string(data_, length()); }
-                
                 StringPiece toStringPiece() const { return StringPiece(data_, length()); }
 
             private:
-                const char* end() const { return data_ + sizeof(data_); }
+                char data_[SIZE];
+                char *cur_;
 
                 static void cookieStart();
                 static void cookieEnd();
 
-                void (*cookie_) ();
-                char  data_[SIZE];
-                char* cur_;
+                const char* end() const 
+                {
+                    return data_ + sizeof(data_);
+                }
         };
+
+
     }
 
     class LogStream : noncopyable
@@ -74,7 +84,7 @@ namespace weasel
         public:
             typedef detail::FixedBuffer<detail::kSmallBuffer> Buffer;
 
-            self& operator<<(bool v)
+            self& operator<<(bool v) 
             {
                 buffer_.append(v ? "1" : "0", 1);
                 return *this;
@@ -88,7 +98,6 @@ namespace weasel
             self& operator<<(unsigned long);
             self& operator<<(long long);
             self& operator<<(unsigned long long);
-            self& operator<<(double);
 
             self& operator<<(const void*);
 
@@ -97,7 +106,9 @@ namespace weasel
                 *this << static_cast<double>(v);
                 return *this;
             }
-            
+
+            self& operator<<(double);
+
             self& operator<<(char v)
             {
                 buffer_.append(&v, 1);
@@ -118,7 +129,7 @@ namespace weasel
 
             self& operator<<(const unsigned char* str)
             {
-                return operator<<(reinterpret_cast<const char*>(str));
+                return operator<<(reintepret_cast<const char*>(str));
             }
 
             self& operator<<(const string& v)
@@ -139,28 +150,18 @@ namespace weasel
                 return *this;
             }
 
-            void append(const char* data, int len)
-            {
-                buffer_.append(data, len);
-            }
+            void append(const char* data, int len) { buffer_.append(data, len); }
+            void resetBuffer() { buffer_.reset(); }
 
-            void resetBuffer() 
-            {
-                buffer_.reset();
-            }
+            const Buffer& buffer() const { return buffer_; }
 
-            const Buffer& buffer() const
-            {
-                return buffer_;
-            }
-            
         private:
+            Buffer buffer_;
+
             void staticCheck();
 
             template <typename T>
             void formatInteger(T);
-
-            Buffer buffer_;
 
             static const int kMaxNumericSize = 48;
     };
@@ -169,14 +170,14 @@ namespace weasel
     {
         public:
             template <typename T>
-            Fmt(const char* fmt, T val);
+            Fmt (const char* fmt, T val);
 
-            const char* data() const { return buf_; }
             int length() const { return length_; }
+            const char* data() const { return buf_; }
 
         private:
-            int  length_;
-            char buf_[32];
+            int length_;
+            char buf_[BUFFER_SIZE];
     };
 
     inline LogStream& operator<<(LogStream& s, const Fmt& fmt)
@@ -185,7 +186,10 @@ namespace weasel
         return s;
     }
 
+    // SI units (K, M, G, T, P, E)
     string formatSI(int64_t n);
+
+    // IEC units (Ki, Mi, Gi, Ti, Pi, Ei)
     string formatIEC(int64_t n);
 }
 
